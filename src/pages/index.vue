a853ca8839c9565356b40428111661ff813cb059
Jon Ganebski <jon.ganebski@gmail.com>
2022-02-03 13:44:50 +0900

    [GPMS-74] Create Default Layout (#3)
    * :sparkles: add gopms logos
    
    * :lipstick: paint default layout
    
    * :sparkles: create pages
    
    * :sparkles: add buttons on the toolbar
    
    * :coffin: clean codes
    
    * :globe_with_meridians: add i18n

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,7 @@
-<script setup lang="ts">
-import { useRouter } from 'vue-router';
-import { useAuth } from '~/composables/useAuth';
-
-const router = useRouter();
-const auth = useAuth();
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
     <h1>Index Page</h1>
-    <q-btn @click="auth.signout(router)">Signout</q-btn>
   </div>
 </template>
---
+++ src/pages/settings/rate-structures.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Under Development</h1>
+  </div>
+</template>
